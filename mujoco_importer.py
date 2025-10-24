82bd3a0c6753b34b7b4b3e6d1d0e146c03105bd2
Weihang Guo <weihang.guo2001@gmail.com>
2025-10-24 11:31:18 -0500

    Remove code for root bodies in armature origin handling

--- mujoco_importer.py
+++ mujoco_importer.py
@@ -563,8 +563,8 @@ class BodyInfo:
         if self._parent is not None:
             tf_parent_in_armature = self._parent.get_initial_tf_in_armature()
         body_in_armature = tf_parent_in_armature * self._body_in_parent_tf
-        if self._parent is not None and self._parent.get_parent() is None:
-            body_in_armature = Transform() # root bodies at armature origin
+        # if self._parent is not None and self._parent.get_parent() is None:
+        #     body_in_armature = Transform() # root bodies at armature origin
         self._initial_tf_in_armature = body_in_armature
         # infer depth
         self._depth = 0
